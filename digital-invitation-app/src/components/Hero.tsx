a1e2e8fcbf54effbd65f768de444bb1e7b3470a1
America <240296.alh@gmail.com>
2025-05-18 01:16:05 -0600

    [Debug]

--- digital-invitation-app/src/components/Hero.tsx
+++ digital-invitation-app/src/components/Hero.tsx
@@ -16,30 +16,28 @@ export default function Hero() {
         />
 
         {/* Text Overlay */}
-        <div className="relative z-[1] items-center">
-          <div className="absolute inset-x-0 bottom-0 justify-center text-white px-4 text-center">
-            <h1
-              className={`${tangerine.className} text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-gold drop-shadow-lg`}
-            >
-              América y Alonso
-            </h1>
+        <div className="absolute inset-x-0 bottom-0 justify-center text-white px-4 text-center">
+          <h1
+            className={`${tangerine.className} text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-gold drop-shadow-lg`}
+          >
+            América y Alonso
+          </h1>
 
-            {/* Cover Leaf Divider Section */}
-            <div className="my-4">
-              <CoverLeafDivider />
-            </div>
-            <p
-              className={`${tangerine.className} text-xl sm:text-2xl md:text-3xl text-gold`}
-            >
-              Sábado, 6 de Diciembre 2025
-            </p>
+          {/* Cover Leaf Divider Section */}
+          <div className="my-4">
+            <CoverLeafDivider />
           </div>
-
-          {/* Play Button */}
-          <button className="absolute bottom-4 right-4 bg-white/70 hover:bg-white/90 p-3 rounded-full shadow-md transition">
-            ▶
-          </button>
+          <p
+            className={`${tangerine.className} text-xl sm:text-2xl md:text-3xl text-gold`}
+          >
+            Sábado, 6 de Diciembre 2025
+          </p>
         </div>
+
+        {/* Play Button */}
+        <button className="absolute bottom-4 right-4 bg-white/70 hover:bg-white/90 p-3 rounded-full shadow-md transition">
+          ▶
+        </button>
       </div>
     </div>
   );
