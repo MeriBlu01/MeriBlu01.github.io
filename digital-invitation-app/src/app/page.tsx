318a670224d9e94c77e234603a0f516623ab8642
America <240296.alh@gmail.com>
2025-05-17 23:52:31 -0600

    [Chore] Refactor Hero Structure

--- digital-invitation-app/src/app/page.tsx
+++ digital-invitation-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center w-full">
+    <main className="overflow-hidden">
       {/* Hero Section */}
       <Hero />
 
--- digital-invitation-app/src/components/Hero.tsx
+++ digital-invitation-app/src/components/Hero.tsx
@@ -4,39 +4,41 @@ import CoverLeafDivider from "@/components/CoverLeafDivider";
 
 export default function Hero() {
   return (
-    <div className="relative w-full h-screen mx-auto overflow-hidden">
-      {/* Background Image */}
-      <Image
-        src="/cover.png"
-        alt="cover"
-        fill
-        className="object-cover sm:object-contain sm:object-[left_50%] md:object-cover"
-        priority
-      />
+    <div className="hero">
+      <div className="flex w-full h-screen mx-auto overflow-hidden">
+        {/* Background Image */}
+        <Image
+          src="/cover.png"
+          alt="cover"
+          fill
+          className="object-cover sm:object-contain sm:object-[left_50%] md:object-cover"
+          priority
+        />
 
-      {/* Text Overlay */}
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-4 text-center">
-        <h1
-          className={`${tangerine.className} text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-gold drop-shadow-lg`}
-        >
-          América y Alonso
-        </h1>
+        {/* Text Overlay */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-4 text-center">
+          <h1
+            className={`${tangerine.className} text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-gold drop-shadow-lg`}
+          >
+            América y Alonso
+          </h1>
 
-        {/* Cover Leaf Divider Section */}
-        <div className="my-4">
-          <CoverLeafDivider />
+          {/* Cover Leaf Divider Section */}
+          <div className="my-4">
+            <CoverLeafDivider />
+          </div>
+          <p
+            className={`${tangerine.className} text-xl sm:text-2xl md:text-3xl text-gold`}
+          >
+            Sábado, 6 de Diciembre 2025
+          </p>
         </div>
-        <p
-          className={`${tangerine.className} text-xl sm:text-2xl md:text-3xl text-gold`}
-        >
-          Sábado, 6 de Diciembre 2025
-        </p>
-      </div>
 
-      {/* Play Button */}
-      <button className="absolute bottom-4 right-4 bg-white/70 hover:bg-white/90 p-3 rounded-full shadow-md transition">
-        ▶
-      </button>
+        {/* Play Button */}
+        <button className="absolute bottom-4 right-4 bg-white/70 hover:bg-white/90 p-3 rounded-full shadow-md transition">
+          ▶
+        </button>
+      </div>
     </div>
   );
 }
