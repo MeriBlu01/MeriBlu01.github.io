8276fd92a6dae314a136ca9a30b82f3eeb476b43
America <240296.alh@gmail.com>
2025-05-17 21:25:25 -0600

    [FIX] Note: no more next export to deploy static page

--- digital-invitation-app/next.config.ts
+++ digital-invitation-app/next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-const repoName = "wedding-invitation";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/wedding-invitation",
-  assetPrefix: "/wedding-invitation/",
 };
 
 export default nextConfig;
