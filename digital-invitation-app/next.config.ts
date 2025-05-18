8a6dc889db0d3065ebf3d94ea85694c9cda328c6
America <240296.alh@gmail.com>
2025-05-17 23:06:28 -0600

    [FEAT] New pipeline

--- digital-invitation-app/next.config.ts
+++ digital-invitation-app/next.config.ts
@@ -4,8 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/wedding-invitation",
-  assetPrefix: "/wedding-invitation/",
+  assetPrefix: "/wedding-invitation/digital-invitation-app/public",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
