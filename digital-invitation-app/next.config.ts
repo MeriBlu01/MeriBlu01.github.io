28e123a698eab346dbf5aa5637d864fce8097afd
America <240296.alh@gmail.com>
2025-05-17 22:31:52 -0600

    [Debug]

--- digital-invitation-app/next.config.ts
+++ digital-invitation-app/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/wedding-invitation",
+  assetPrefix: "/wedding-invitation/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
