63afbfc556fe10d0591d58e870d00671dd157277
America <240296.alh@gmail.com>
2025-05-20 01:11:43 -0600

    [Chore] Hero Cleanup

--- digital-invitation-app/src/components/Hero.tsx
+++ digital-invitation-app/src/components/Hero.tsx
@@ -19,30 +19,33 @@ export default function Hero() {
         priority
       />
 
-      <div className="absolute w-3/4 left-0 right-0 top-[40vh] md:top-[50vh] mx-auto flex flex-col items-center justify-start text-white text-center">
+      {/* Text Overlay */}
+      <div className="absolute w-3/4 left-0 right-0 top-[45vh] md:top-[50vh] gap-y-20 md:gap-y-40 mx-auto flex flex-col items-center justify-start text-white text-center">
         {/* Header Overlay */}
         <h1
-          className={`${tangerine.className} text-6xl font-bold sm:text-6xl md:text-7xl lg:text-9xl flex flex-col md:flex-row gap-x-2 md:gap-x-4 justify-end`}
+          className={`${tangerine.className} text-6xl font-bold md:text-7xl lg:text-9xl flex flex-col md:flex-row gap-x-2 md:gap-x-4`}
         >
           <span>América</span>
           <span>&</span>
           <span>Alonso</span>
         </h1>
 
-        {/* Cover Leaf Divider Section */}
-        <CoverLeafDivider />
-
-        {/* Text Overlay */}
         <p
           className={`${tangerine.className} text-3xl font-bold text-light-glow-gold md:text-4xl lg:text-5xl`}
         >
           Sábado, 6 de Diciembre 2025
         </p>
       </div>
+
+      {/* Cover Leaf Divider */}
+      <div className="absolute flex w-3/4 left-0 right-0 top-[67vh] md:top-[57vh] mx-auto justify-center">
+        <CoverLeafDivider />
+      </div>
+
       {/* Music Icon Button */}
-      <div className="absolute bottom-4 right-4 size-12 sm:size-12 md:size-16">
+      <div className="absolute bottom-4 right-4 size-12 md:size-16">
         <button
-          onClick={() => setActive(!active)}
+          onClick={() => setActive((prev) => !prev)}
           className={`relative group rounded-lg transition duration-300
             ${active ? "bg-white/90" : "bg-white/70 hover:bg-white/90"}`}
         >
