ab6c412a1463f53d8ffe36c1d31df0e2532b1f13
America <240296.alh@gmail.com>
2025-05-20 00:37:12 -0600

    [Chore] Update Hero Layout

--- digital-invitation-app/src/components/CoverLeafDivider.tsx
+++ digital-invitation-app/src/components/CoverLeafDivider.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function CoverLeafDivider() {
   return (
-    <div className="flex items-center gap-x-8 md:gap-x-16 w-3/4 sm:w-3/4 md:w-1/2">
+    <div className="flex gap-x-8 md:gap-x-16 w-full h-full md:h-2/3 md:w-3/4 mb-10 md:mb-0">
       {/* Normal Leaf */}
       <div className="relative aspect-[2/1] w-1/2">
         <Image
-          src="/Vectors/leaf_left.svg"
+          src="/Vectors/leaf_left_white.svg"
           alt="Left Leaf Divider"
           fill
           className="object-contain"
@@ -16,7 +16,7 @@ export default function CoverLeafDivider() {
       {/* Mirrored Leaf */}
       <div className="relative aspect-[2/1] w-1/2">
         <Image
-          src="/Vectors/leaf_right.svg"
+          src="/Vectors/leaf_right_white.svg"
           alt="Mirrored Leaf Divider"
           fill
           className="object-contain"
--- digital-invitation-app/src/components/Hero.tsx
+++ digital-invitation-app/src/components/Hero.tsx
@@ -1,52 +1,57 @@
+"use client"; // Required only if you're using Next.js App Router
+
+import { useState } from "react";
 import Image from "next/image";
 import { tangerine } from "@/lib/fonts";
 import CoverLeafDivider from "@/components/CoverLeafDivider";
 
 export default function Hero() {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="hero">
-      <div className="flex flex-col w-full h-screen mx-auto">
-        {/* Background Image */}
-        <Image
-          src="/Images/cover.png"
-          alt="cover"
-          fill
-          className="relative object-cover brightness-75 sm:object-top-left"
-          sizes="100vw"
-          priority
-        />
+    <div className="relative w-full h-dvh flex flex-col">
+      {/* Background Image */}
+      <Image
+        src="/Images/cover.png"
+        alt="cover"
+        fill
+        className="object-cover object-[35%_50%] brightness-75"
+        priority
+      />
 
-        <div className="absolute inset-x-0 bottom-0 md:bottom-14 h-1/2 md:h-2/5 flex flex-col items-center justify-start text-white text-center">
-          {/* Header Overlay */}
-          <h1
-            className={`${tangerine.className} text-6xl font-bold sm:text-6xl md:text-7xl lg:text-9xl flex flex-col sm:flex-row gap-2 sm:gap-4`}
-          >
-            <span>América</span>
-            <span>&</span>
-            <span>Alonso</span>
-          </h1>
+      <div className="absolute w-3/4 left-0 right-0 top-[40vh] md:top-[50vh] mx-auto flex flex-col items-center justify-start text-white text-center">
+        {/* Header Overlay */}
+        <h1
+          className={`${tangerine.className} text-6xl font-bold sm:text-6xl md:text-7xl lg:text-9xl flex flex-col md:flex-row gap-x-2 md:gap-x-4 justify-end`}
+        >
+          <span>América</span>
+          <span>&</span>
+          <span>Alonso</span>
+        </h1>
 
-          {/* Cover Leaf Divider Section */}
-          <CoverLeafDivider />
+        {/* Cover Leaf Divider Section */}
+        <CoverLeafDivider />
 
-          {/* Text Overlay */}
-          <p
-            className={`${tangerine.className} mt-11 md:mt-0 text-4xl font-bold text-light-glow-gold sm:text-4xl md:text-4xl lg:text-5xl`}
-          >
-            Sábado, 6 de Diciembre 2025
-          </p>
-        </div>
-
-        {/* Music Icon Button */}
-        <div className="absolute bottom-4 right-4 size-12 sm:size-12 md:size-16">
-          <button className="relative group rounded-lg bg-white/70 hover:bg-white/90 transition">
-            <img
-              src="/Vectors/Icon-Music.svg"
-              className="fill-current"
-              alt="Music Icon"
-            />
-          </button>
-        </div>
+        {/* Text Overlay */}
+        <p
+          className={`${tangerine.className} text-4xl font-bold text-light-glow-gold md:text-4xl lg:text-5xl`}
+        >
+          Sábado, 6 de Diciembre 2025
+        </p>
+      </div>
+      {/* Music Icon Button */}
+      <div className="absolute bottom-4 right-4 size-12 sm:size-12 md:size-16">
+        <button
+          onClick={() => setActive(!active)}
+          className={`relative group rounded-lg transition duration-300
+            ${active ? "bg-white/90" : "bg-white/70 hover:bg-white/90"}`}
+        >
+          <img
+            src="/Vectors/Icon-Music.svg"
+            alt="Music Icon"
+            className={`fill-current ${active ? "animate-pulse" : ""}`}
+          />
+        </button>
       </div>
     </div>
   );
