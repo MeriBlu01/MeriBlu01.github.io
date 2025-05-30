d0778c314e253aa9373a2b78e5b2429d0157d828
America <240296.alh@gmail.com>
2025-05-30 11:17:53 -0600

    [Chore] Map section added

--- digital-invitation-app/src/components/TimeAndLocation.tsx
+++ digital-invitation-app/src/components/TimeAndLocation.tsx
@@ -15,7 +15,7 @@ export default function TimeAndLocation() {
           priority
         />
       </div>
-      <div className="w-full flex flex-col justify-center text-center gap-y-5">
+      <div className="w-full flex flex-col items-center justify-center text-center gap-y-5">
         <p
           className={`${simonetta.className} font-normal text-black text-[24px]`}
         >
@@ -27,15 +27,26 @@ export default function TimeAndLocation() {
           Hacienda de Negrete
         </h1>
         <p
-          className={`${simonetta.className} font-normal text-black text-[24px] text-center`}
+          className={`${simonetta.className} font-normal text-black text-[24px]`}
         >
           Te esperamos el día:
         </p>
         <p
-          className={`${tangerine.className} font-bold text-red-brown text-[48px] text-center`}
+          className={`${tangerine.className} font-bold text-red-brown text-[48px]`}
         >
           06 de Diciembre 2025 - 06:00 p.m.*
         </p>
+        <div className="relative w-2/5 aspect-[400/300] flex flex-col justify-center items-center">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.9200366343266!2d-103.65962260946154!3d19.28584328564265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x84255a35a672f383%3A0x149b38d9ae15fa89!2sHacienda%20de%20Negrete!5e0!3m2!1ses-419!2smx!4v1748624566939!5m2!1ses-419!2smx"
+            width="100%"
+            height="100%"
+            className="border-yellow-3 rounded-[16px]"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
