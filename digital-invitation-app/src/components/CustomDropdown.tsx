0acf5d29201aca0799a428e08b8b55954410e129
America <240296.alh@gmail.com>
2025-06-02 00:57:52 -0600

    [Chore] Form section (Form, CustomDropdown) added and resized to fit mobile screens

---
+++ digital-invitation-app/src/components/CustomDropdown.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { ChevronDown } from "lucide-react";
+
+type DropdownProps = {
+  options: string[];
+  initial: string;
+  onChange?: (value: string) => void;
+};
+
+export default function CustomDropdown({
+  options,
+  initial,
+  onChange,
+}: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null); // <- ref to wrapper
+
+  const handleSelect = (option: string) => {
+    setIsOpen(false);
+    onChange?.(option); // optional callback
+  };
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div ref={dropdownRef} className="relative w-full">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="bg-yellow-4 text-white px-6 py-3 w-full rounded-md flex justify-between items-center text-lg"
+      >
+        {initial}
+        <ChevronDown className="size-[28px] ml-2" />
+      </button>
+
+      {isOpen && (
+        <ul className="absolute w-full mt-2 bg-neutral rounded-md shadow-md z-10">
+          {options.map((option, idx) => (
+            <li
+              key={idx}
+              onClick={() => handleSelect(option)}
+              className="px-6 py-2 hover:bg-moon-mist cursor-pointer"
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
--- digital-invitation-app/src/components/Form.tsx
+++ digital-invitation-app/src/components/Form.tsx
@@ -1,61 +1,153 @@
+"use client";
+
 import Image from "next/image";
 import { simonetta } from "@/lib/fonts";
 import { tangerine } from "@/lib/fonts";
+import CustomDropdown from "@/components/CustomDropdown";
 
 export default function Form() {
+  const handleChange = (value: string) => {
+    console.log("Selected:", value);
+  };
+
   return (
-    <div className="bg-yellow-2 w-full h-fit p-12 md:p-15 flex flex-col justify-between items-center">
+    <section className="bg-yellow-2 w-full h-fit p-4 md:p-15 space-y-14">
       {/* Confirm Attendance Header */}
-      <div className="flex justify-center">
-        <div className="flex gap-10 items-center w-fit mx-auto">
-          <h1
-            className={`${tangerine.className} font-extrabold text-green-1 text-[42px] md:text-[74px]`}
-          >
-            Confirmar asistencia
-          </h1>
-        </div>
-      </div>
-      <div className="relative w-[100px] md:w-1/6 aspect-[101/146]">
-        <Image
-          src="/Vectors/Detail-leaves-save-date-left.svg"
-          alt="left leaf frame for waiting message"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
-      {/* Message */}
-      <div className="relative w-3/5 md:w-1/2 h-full p-2 gap-4 md:gap-6 flex flex-col items-center justify-center text-center">
+      <div className="flex flex-col justify-center items-center -space-y-3 w-fit mx-auto">
         <h1
-          className={`${tangerine.className} font-extrabold text-green-1 text-[42px] md:text-[74px]`}
+          className={`${tangerine.className} font-extrabold text-center text-yellow-4 text-[42px] md:text-[74px]`}
         >
-          Te esperamos...
+          Confirmar asistencia
         </h1>
-        <p
-          className={`${simonetta.className} font-normal text-black text-xl md:text-[30px]`}
-        >
-          ¡NO FALTES!
-        </p>
-        <div className="relative w-[50px] md:w-1/5 aspect-[50/32] flex justify-center items-center">
+        <div className="relative w-[180px] md:w-[260px] aspect-[5/2]">
           <Image
-            src="/Vectors/Vector-I-rings.svg"
-            alt="Rings icon"
+            src="/Vectors/Leafs_Form.svg"
+            alt="Leafs Form Divider"
             fill
             className="object-contain"
             priority
           />
         </div>
       </div>
-      {/* Left Leaves Frame */}
-      <div className="relative w-[100px] md:w-1/6 aspect-[101/146]">
-        <Image
-          src="/Vectors/Detail-leaves-save-date-right.svg"
-          alt="Right leaf frame for waiting message"
-          fill
-          className="object-contain"
-          priority
-        />
+      {/* Instructions */}
+      <div className="w-full text-center">
+        <p
+          className={`${simonetta.className} font-normal text-black text-xl md:text-[24px]`}
+        >
+          Llena el siguiente formulario y no olvides dar click en el botón de
+          enviar, nosotros revisaremos tu confirmación y te agregaremos a
+          nuestra lista de invitados.
+          <br></br>
+          <br></br>
+          Te pedimos ser respetuoso con los lugares que ya hemos asignado para
+          tí en el pase de entrada que te hemos enviado.
+        </p>
       </div>
-    </div>
+      {/* Guest Form */}
+      <form
+        className={`${simonetta.className} w-full max-w-md mx-auto space-y-6`}
+        onSubmit={(e) => {
+          e.preventDefault();
+          // Add your submit logic here
+          console.log("Form submitted");
+        }}
+      >
+        {/* Full Name */}
+        <div>
+          <label htmlFor="name" className="block text-lg mb-1 text-yellow-4">
+            Nombre completo
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            className="w-full border border-yellow-4 bg-neutral p-3 rounded-sm focus:outline-none focus:ring-2 focus:ring-yellow-4"
+          />
+        </div>
+
+        {/* Phone Number */}
+        <div>
+          <label className="block text-lg mb-1 text-yellow-4">
+            Número telefónico
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            required
+            className="w-full border border-yellow-4 bg-neutral p-3 rounded-sm focus:outline-none focus:ring-2 focus:ring-yellow-4"
+          />
+        </div>
+
+        <CustomDropdown
+          options={["1", "2", "3", "4"]}
+          initial="Número de invitados"
+          onChange={handleChange}
+        />
+        <CustomDropdown
+          options={["Sí, asistiré", "No, no podré asistir"]}
+          initial="Confirmación de asistencia"
+          onChange={handleChange}
+        />
+
+        {/* Congratulatory Messagee */}
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-lg mt-8 mb-1 text-yellow-4"
+          >
+            Mensaje de felicitación
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows={5}
+            className="w-full border border-yellow-4 bg-neutral p-6 rounded-sm focus:outline-none focus:ring-2 focus:ring-yellow-4 resize-none"
+          />
+        </div>
+
+        {/* Allergies */}
+        <div>
+          <label className="block text-lg text-yellow-4">Alergias</label>
+          <div className="flex justify-left pt-4 pb-4 gap-4">
+            <label className="flex items-center gap-2">
+              <input
+                type="radio"
+                name="allergy"
+                value="yes"
+                className="accent-yellow-400 size-[20px]"
+              />
+              Sí, padezco de alergias
+            </label>
+            <label className="flex items-center gap-2">
+              <input
+                type="radio"
+                name="allergy"
+                value="no"
+                className="accent-yellow-400 size-[20px]"
+              />
+              No, no tengo alergias
+            </label>
+          </div>
+
+          <input
+            type="text"
+            placeholder="Escribe aquí tus alergias alimentarias"
+            className="w-full border border-yellow-4 bg-neutral p-3 rounded-sm focus:outline-none focus:ring-2 focus:ring-[#4A4A00]"
+          />
+        </div>
+
+        {/* Submit Button */}
+        <div className="flex justify-center pb-[50px]">
+          <button
+            type="submit"
+            className="bg-yellow-4 hover:bg-amber-500 transition-colors text-white px-6 py-3 rounded-md mt-6"
+          >
+            Enviar
+          </button>
+        </div>
+      </form>
+    </section>
   );
 }
