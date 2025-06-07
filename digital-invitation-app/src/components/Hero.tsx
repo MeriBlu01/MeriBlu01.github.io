1ba2a83287396ab46bb73c7b4a88cdabdca60b35
America <240296.alh@gmail.com>
2025-06-07 12:49:01 -0600

    [Fix] Try 1: Fix Hero section responsiveness for mobile devices

--- digital-invitation-app/src/components/Hero.tsx
+++ digital-invitation-app/src/components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
 
   return (
     <div className="relative w-full h-dvh flex flex-col">
-      <div className="relative h-full aspect-[5861/3907]">
+      <div className="h-full aspect-[5861/3907]">
         {/* Background Image */}
         <Image
           src="/Images/cover.png"
@@ -21,27 +21,28 @@ export default function Hero() {
         />
       </div>
 
-      {/* Text Overlay */}
-      <div className="absolute w-3/4 left-0 right-0 top-[45vh] md:top-[50vh] gap-y-20 md:gap-y-40 mx-auto flex flex-col items-center justify-start text-white text-center">
-        {/* Header Overlay */}
-        <h1
-          className={`${tangerine.className} text-6xl font-bold md:text-7xl lg:text-9xl flex flex-col md:flex-row gap-x-2 md:gap-x-4`}
-        >
-          <span>América</span>
-          <span>&</span>
-          <span>Alonso</span>
-        </h1>
+      <div className="absolute flex flex-col justify-center w-3/4 left-1/2 transform -translate-x-1/2 top-[50vh]">
+        {/* Text Overlay */}
+        <div className="relative w-full flex flex-col items-center justify-start -space-y-5 md:-space-y-15 text-white text-center">
+          {/* Header Overlay */}
+          <h1
+            className={`${tangerine.className} text-6xl font-bold md:text-7xl lg:text-9xl flex flex-col md:flex-row gap-x-2 md:gap-x-4`}
+          >
+            <span>América</span>
+            <span>&</span>
+            <span>Alonso</span>
+          </h1>
 
+          {/* Cover Leaf Divider */}
+          <CoverLeafDivider />
+        </div>
+
+        {/* Date Overlay */}
         <p
-          className={`${tangerine.className} text-3xl font-bold text-light-glow-gold md:text-4xl lg:text-5xl`}
+          className={`${tangerine.className} text-3xl font-bold text-center text-white text-light-glow-gold md:text-4xl lg:text-5xl`}
         >
           Sábado, 6 de Diciembre 2025
         </p>
-      </div>
-
-      {/* Cover Leaf Divider */}
-      <div className="absolute flex w-3/4 left-0 right-0 top-[67vh] md:top-[57vh] mx-auto justify-center">
-        <CoverLeafDivider />
       </div>
 
       {/* Music Icon Button */}
