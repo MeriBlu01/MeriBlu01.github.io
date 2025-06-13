75ad0d08f2f62a27a6b02b23087a50d48b416d23
Natalia Ortiz <nsol1094@gmail.com>
2025-06-13 00:35:50 -0600

    Invitation section update, responsive behavior - refs #25

--- digital-invitation-app/src/components/InvitationTitle.tsx
+++ digital-invitation-app/src/components/InvitationTitle.tsx
@@ -9,48 +9,52 @@ export default function Invitation() {
       id="invitation"
       className=" scroll-mt-[50px] w-full sm:px-[16px] lg:px-[80px] max-w-[1730px] mx-auto"
     >
-      <div className="bg-yellow-2 w-full h-fit lg:p-[42] gap-[16px] flex flex-col justify-center rounded-[16px] invitation-background">
+      <div className="bg-yellow-2 w-full h-fit flex flex-col justify-center rounded-[16px] md: lg:p-[42] lg:gap-[16px] invitation-background">
         <div className="w-full lg:px-[80px] flex flex-col justify-center">
           <h1
-            className={`${tangerine.className} font-extrabold text-yellow-4 text-[48px] text-center`}
+            className={`${tangerine.className} font-extrabold text-yellow-4 text-center text-[40px] sm:text-[48px] md:text-[64px] lg:text-[72px] xl:text-[80px]`}
           >
             ¡Nos casamos!{" "}
           </h1>
           <span className="w-full flex flex-col gap-[24px] lg:px-[80px]">
-            <p className={`${simonetta.className} font-normal text-black text-[24px] text-center`}>
+            <p
+              className={`${simonetta.className} font-normal text-black text-center px-[40px] text-[16px] sm:text-[24px] xl:text-[32px] `}
+            >
               &quot;Lo más grande que aprenderás en la vida será a amar y ser amado de vuelta&quot;{" "}
               <br />
               Moulin Rouge
             </p>
-            <p className={`${simonetta.className} font-normal text-black text-[24px] text-center`}>
+            <p
+              className={`${simonetta.className} font-normal text-black text-center px-[56px] text-[16px] sm:text-[24px] xl:text-[32px] `}
+            >
               Queremos compartir contigo la alegría de nuestra unión, y nos gustaría que nos
               acompañases a celebrar este momento tan especial para nosotros
             </p>
             <div className="w-full py-[24px] gap-[8px]">
               <p
-                className={`${simonetta.className} font-normal text-black text-[24px] text-center`}
+                className={`${simonetta.className} font-normal text-black text-center text-[16px] sm:text-[24px] xl:text-[32px] `}
               >
                 Invitacion valida para:
               </p>
               <p
-                className={`${tangerine.className} font-bold text-red-brown text-[40px] text-center`}
+                className={`${tangerine.className} font-bold text-red-brown text-center text-[32px] sm:text-[40px] xl:text-[56px] `}
               >
                 Nombre de invitado
               </p>
               <div className="flex flex-row justify-center align-middle gap-[8px]">
                 <p
-                  className={`${simonetta.className} font-normal text-yellow-4 text-[32px] text-center`}
+                  className={`${simonetta.className} font-normal text-yellow-4 text-center text-[16px] sm:text-[24px] xl:text-[32px] `}
                 >
                   #
                 </p>
                 <p
-                  className={`${simonetta.className} font-normal text-black  text-[24px] text-center`}
+                  className={`${simonetta.className} font-normal text-black text-center text-[16px] sm:text-[24px] xl:text-[32px] `}
                 >
                   pases disponibles
                 </p>
               </div>
               <p
-                className={`${tangerine.className} font-extrabold text-yellow-4 text-[48px] text-center`}
+                className={`${tangerine.className} font-extrabold text-yellow-4 text-center text-[32px] sm:text-[40px] md:text-[56px] lg:text-[64px]`}
               >
                 ¡Te esperamos!{" "}
               </p>
