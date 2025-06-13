45949b4e158bb39f8bf9e52ac862ef2ab0d13a0d
Natalia Ortiz <nsol1094@gmail.com>
2025-06-13 03:23:17 -0600

    Updated Schedule section, config component with responsive behavior - refs #26

--- digital-invitation-app/src/components/BigPicture.tsx
+++ digital-invitation-app/src/components/BigPicture.tsx
@@ -5,18 +5,17 @@ interface InvitationSectionProps {
   altText: string;
 }
 
-export default function BigPicture({
-  imageSrc,
-  altText,
-}: InvitationSectionProps) {
+export default function BigPicture({ imageSrc, altText }: InvitationSectionProps) {
   return (
     <section className="relative w-full h-96 md:h-dvh flex flex-col items-center justify-center">
-      <div className="absolute mx-auto top-3 md:top-0 w-3/4 md:w-1/3 h-1/6">
+      <div className="absolute flex flex-col justify-center items-center mx-auto top-3 md:top-0 w-3/4 md:w-1/3 h-1/6">
         <Image
           src="/Vectors/Detail-leaves-bot.svg"
           alt="Top leaves decoration"
-          fill
-          className="rotate-180 object-contain"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="rotate-180 min-w-[400px] sm:min-w-[500px] md:min-w-[600px] lg:min-w-[800px] h-auto "
           priority
         />
       </div>
@@ -29,12 +28,14 @@ export default function BigPicture({
           priority
         />
       </div>
-      <div className="absolute mx-auto bottom-3 md:bottom-0 w-3/4 md:w-1/3 h-1/6">
+      <div className="absolute flex flex-col justify-center items-center mx-auto bottom-3 md:bottom-0 w-3/4 md:w-1/3 h-1/6">
         <Image
           src="/Vectors/Detail-leaves-bot.svg"
           alt="Bottom leaves decoration"
-          fill
-          className="object-contain"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="min-w-[400px] sm:min-w-[500px] md:min-w-[600px]  lg:min-w-[800px] h-auto "
           priority
         />
       </div>
--- digital-invitation-app/src/components/ScheduleCard.tsx
+++ digital-invitation-app/src/components/ScheduleCard.tsx
@@ -3,69 +3,159 @@ import { JSX } from "react";
 import { simonetta, tangerine } from "@/lib/fonts";
 import Image from "next/image";
 
-type ScheduleIcon={
-    Icon: ()=>JSX.Element;
-    title:string;
-    hour:string
-}
+type ScheduleIcon = {
+  Icon: () => JSX.Element;
+  title: string;
+  hour: string;
+};
 
-const titleIcons: ScheduleIcon[]=[
-    {
-        Icon: ()=>( <Image src="/Vectors/Vector-I-rings.svg" alt="Rings" width={100} height={63}/>),
-        title: "Ceremonia",
-        hour: "5:00 pm" 
-    },
-    {
-        Icon: ()=>( <Image src="/Vectors/Vector-I-camera.svg" alt="Photos" width={100} height={72.62}/>),
-        title: "Sesión de fotos",
-        hour: "6:30 pm" 
-    },
-    {
-        Icon: ()=>( <Image src="/Vectors/Vector-I-floral-arch.svg" alt="Rings" width={100} height={107.67}/>),
-        title: "Recepción",
-        hour: "7:00 pm" 
-    },
-    {
-        Icon: ()=>( <Image src="/Vectors/Vector-I-drinking.svg" alt="Toast" width={100} height={119.17}/>),
-        title: "Brindis",
-        hour: "8:00 pm" 
-    },
-    {
-        Icon: ()=>( <Image src="/Vectors/Vector-I-Dish.svg" alt="Dinner" width={125} height={96.1}/>),
-        title: "Cena",
-        hour: "8:30 pm" 
-    },
-    {
-        Icon: ()=>( <Image src="/Vectors/Vector-I-couple.svg" alt="Couple" width={65} height={118.66}/>),
-        title: "Primer baile",
-        hour: "9:00 pm" 
-    },
-    {
-        Icon: ()=>( <Image src="/Vectors/Vector-I-party.svg" alt="Party" width={100} height={114.74}/>),
-        title: "Fiesta",
-        hour: "9:30 pm" 
-    }
-]
+const titleIcons: ScheduleIcon[] = [
+  {
+    Icon: () => (
+      <Image
+        src="/Vectors/Vector-I-rings.svg"
+        alt="Rings"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[150px] h-auto "
+      />
+    ),
+    title: "Ceremonia",
+    hour: "5:00 pm",
+  },
+  {
+    Icon: () => (
+      <Image
+        src="/Vectors/Vector-I-camera.svg"
+        alt="Photos"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[150px] h-auto "
+      />
+    ),
+    title: "Sesión de fotos",
+    hour: "6:30 pm",
+  },
+  {
+    Icon: () => (
+      <Image
+        src="/Vectors/Vector-I-floral-arch.svg"
+        alt="floral-arch"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[130px] h-auto "
+      />
+    ),
+    title: "Recepción",
+    hour: "7:00 pm",
+  },
+  {
+    Icon: () => (
+      <Image
+        src="/Vectors/Vector-I-drinking.svg"
+        alt="Toast"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[130px] h-auto "
+      />
+    ),
+    title: "Brindis",
+    hour: "8:00 pm",
+  },
+  {
+    Icon: () => (
+      <Image
+        src="/Vectors/Vector-I-Dish.svg"
+        alt="Dinner"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[155px] h-auto "
+      />
+    ),
+    title: "Cena",
+    hour: "8:30 pm",
+  },
+  {
+    Icon: () => (
+      <Image
+        src="/Vectors/Vector-I-couple.svg"
+        alt="Couple"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[90px] h-auto "
+      />
+    ),
+    title: "Primer baile",
+    hour: "9:00 pm",
+  },
+  {
+    Icon: () => (
+      <Image
+        src="/Vectors/Vector-I-party.svg"
+        alt="Party"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-[150px] h-auto "
+      />
+    ),
+    title: "Fiesta",
+    hour: "9:30 pm",
+  },
+];
 
-export default function Schedule(): JSX.Element{
-    return(
-        <section id="itinerary" className=" scroll-mt-[50px] w-full bg-yellow-2 gap-0 ">
-            <div className="w-full flex flex-col justify-center lg:px-[80px] py-[32px]  max-w-[1730px] mx-auto">
-
-                <h1 className={`${tangerine.className} font-extrabold text-center text-[48px] text-yellow-4`}>Itinerario</h1>
-                <Image src="/Vectors/Detail-flower.svg" alt="flower" width={151.48} height={75.97} className="mx-auto" />
-                <div className="flex flex-row justify-between ">
-                    {titleIcons.map((element, index)=>(
-                        <div 
-                        key={index}
-                        className="flex flex-col justify-end items-center gap-1 ">
-                            {element.Icon()}
-                            <p className={`${tangerine.className} text-black font-black text-[30px]`}>{element.title}</p>
-                            <p className={`${simonetta.className} text-yellow-4 font-medium text-[20px]`}>{element.hour}</p>
-                        </div>
-                    ))}
-                </div>
+export default function Schedule(): JSX.Element {
+  return (
+    <section id="itinerary" className=" scroll-mt-[50px] w-full bg-yellow-2 gap-0 ">
+      <div className="w-full flex flex-col justify-center items-center px-[16px]  md:px-[56px] lg:px-[80px] py-[32px]  max-w-[1730px] mx-auto">
+        <h1
+          className={`${tangerine.className} font-extrabold text-center text-yellow-4 text-[40px] sm:text-[48px] md:text-[64px] lg:text-[72px] xl:text-[80px]`}
+        >
+          Itinerario
+        </h1>
+        <Image
+          src="/Vectors/Detail-flower.svg"
+          alt="flower"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className=" min-w-[150px] md:min-w-[200px] lg:min-w-[225px]  h-auto "
+        />
+        <div className="w-full flex flex-col gap-[32px] lg:flex-row justify-between lg:gap-0 ">
+          {titleIcons.map((element, index) => (
+            <div
+              key={index}
+              className={`
+                w-full
+                flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"}
+                justify-center items-center
+                lg:flex-col lg:justify-end lg:items-center
+                gap-[24px]
+                `}
+            >
+              {element.Icon()}
+              <div className="w-fit h-fit flex flex-col justify-center items-center">
+                <p
+                  className={`${tangerine.className} text-black font-black text-[32px]  xl:text-[48px] `}
+                >
+                  {element.title}
+                </p>
+                <p
+                  className={`${simonetta.className} text-yellow-4 font-medium text-[16px] sm:text-[24px] xl:text-[32px] pb-[26px] `}
+                >
+                  {element.hour}
+                </p>
+              </div>
             </div>
-        </section>
-    )
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
